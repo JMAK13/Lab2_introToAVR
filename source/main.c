4b300e6ca84b45c9deda45af55f91da87932ebb0
Joshua McIntyre <jmcin005@wch136-31.cs.ucr.edu>
2020-01-09 13:52:36 -0800

    Complete up to Reading from Input Pins

--- source/main.c
+++ source/main.c
@@ -13,8 +13,13 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;	// Configure port B's 8 pins as outputs
+    unsigned char temp_input = 0x00;
+    while(1) {
+        temp_input = PINA;
+        PORTB = temp_input;
+    }    
     /* Insert your solution below */
     while (1) {
 
