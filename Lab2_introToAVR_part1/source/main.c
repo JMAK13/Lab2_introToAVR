9c616dcc92723c2d792f5d7f0d41dde22448b82f
Joshua McIntyre <jmcin005@wch136-13.cs.ucr.edu>
2020-01-21 13:48:26 -0800

    Prepare part1 for ATMega use

--- Lab2_introToAVR_part1/source/main.c
+++ Lab2_introToAVR_part1/source/main.c
@@ -15,17 +15,16 @@
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF;  // Configure port A's 8 pins as inputs
     DDRB = 0xFF; PORTB = 0x00;	// Configure port B's 8 pins as outputs
-    unsigned char tmpA = 0x00;
-    unsigned char tmpB = 0x00;
+    unsigned char led = 0x00;
+    unsigned char button = 0x00;
     while(1) {
-        tmpA = PINA & 0x03;     // Read input
-        tmpB = 0x00;
-        if(tmpA == 0x01) {
-            tmpB = (tmpB & 0xFE) | tmpA;
+        button = ~PINA & 0x01;     // Read input
+        if(button) {
+            led = (led & 0xFC) | 0x01;
         } else {
-            tmpB = (tmpB & 0x00);
+            led = (led & 0xFC) | 0x02;
         }
-        PORTB = tmpB;
+        PORTB = led;
     }    
 
     return 1;
